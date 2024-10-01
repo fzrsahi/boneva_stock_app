e447c372fa893d4830412a680107a7434df3be9f
matzgans <rahmatlahay84@gmail.com>
2024-10-01 15:44:48 +0800

    first Commit

---
+++ database/seeders/ProductSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Product;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $products = ['Boneva Gelas', 'Boneva Galon', "Boneva Botol"];
+        foreach ($products as $product) {
+            Product::create([
+                'name' => $product,
+                'amount' => fake()->randomNumber(),
+                'price' => fake()->randomNumber(),
+                'date' => fake()->date(),
+            ]);
+        }
+    }
+}
