ce37288d3099d55b99945a696821ea8b6579dd42
VIBEX45 <fberahim2@gmail.com>
2024-10-02 10:47:47 +0800

    button color

--- resources/views/pages/product/index.blade.php
+++ resources/views/pages/product/index.blade.php
@@ -9,7 +9,7 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
                     <button type="button" data-modal-target="crud-modal" data-modal-toggle="crud-modal"
-                        class="focus:outline-none text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Tambah
+                        class="focus:outline-none text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Tambah
                         Produk +</button>
 
                     <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
